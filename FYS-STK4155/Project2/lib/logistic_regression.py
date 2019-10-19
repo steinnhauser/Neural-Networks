8238c8969f4566cc48c92b0fbee697c1d287c2e3
steinnhauser <steinnhauser@mac.com>
2019-10-19 14:25:00 +0200

    Added a Gradient Descent class. All functionalities are still included. Added a "predict" functionality for testing data.

--- FYS-STK4155/Project2/lib/logistic_regression.py
+++ FYS-STK4155/Project2/lib/logistic_regression.py
@@ -14,157 +14,119 @@ def logistic_regression():
     # an argument "mini-batches". This is useful for the Neural Network later.
     pass
 
-def gradient_descent_solver(X, y, x0=0, random_state_x0=False,\
-    gamma_k = 0.1, max_iter=50, tol=1e-2):
-    """
-    Calculates a gradient descent starting from x0.
+class GradientDescent:
+    def __init__(self, x0=0, random_state_x0=False,\
+        gamma_k = 0.01, max_iter=50, tol=1e-5, verbose=False):
+        self.x0 = x0
+        self.gamma_k = gamma_k
+        self.max_iter = max_iter
+        self.tol = tol
+        self.random_state_x0=random_state_x0
+        self.verbose=verbose
 
-    Parameters:
-    -----------
-    x0 : vec
-        Initial guess for the minimum of F
-    gamma_k : float
-        Learning rate of the solver ('step size' of delF).
-    max_iter : int
-        Maximum amount of iterations before exiting.
-    tol : float
-        Tolerance which dictates when an answer is sufficient.
+    def solve(self, X, y):
+        self.X = X
+        self.y = y
+        self.gradient_descent_solver()
 
-    Returns:
-    --------
-    xsol : vec
-        Vector which produces a minimum of F.
-    """
-    if gamma_k <= 0:
-        raise ValueError("Bad useage:\n\tThe learning rate is negative.")
+    def gradient_descent_solver(self):
+        """
+        Calculates a gradient descent starting from x0.
 
-    if random_state_x0:
-        preds = X.shape[1] # p
-        xsol = (np.random.random(preds) - 0.5)*0.7/0.5 # between [-0.7, 0.7]
-        if not type(x0) == int:
-            if not np.equal(x0.all(), 0):
-                print("Useage Warning: Overwriting set x0 with random values.")
-        elif type(x0) == int and x0!=0:
-            print(f"Useage Warning: Overwriting set x0={x0} with random values")
+        Parameters:
+        -----------
+        x0 : vec
+            Initial guess for the minimum of F
+        gamma_k : float
+            Learning rate of the solver ('step size' of delF).
+        max_iter : int
+            Maximum amount of iterations before exiting.
+        tol : float
+            Tolerance which dictates when an answer is sufficient.
 
-    else:
-        if type(x0) == int:
-            if x0:
-                xsol = np.ones(X.shape[1])*x0
-            else:
-                xsol=np.zeros(X.shape[1])
-        elif type(x0) == np.ndarray:
-            if x0.shape[0]==X.shape[1]: # if len = p
-                xsol = x0
-            else:
-                raise ValueError("Bad useage: x0 was not of length 1 or p.")
+        Returns:
+        --------
+        xsol : vec
+            Vector which produces a minimum of F.
+        """
+        if self.gamma_k <= 0:
+            raise ValueError("Bad useage:\n\tThe learning rate is negative.")
+
+        if self.random_state_x0:
+            preds = X.shape[1] # p
+            self.xsol = (np.random.random(preds) - 0.5)*0.7/0.5 # between [-0.7, 0.7]
+            if not type(self.x0) == int:
+                if not np.equal(self.x0.all(), 0):
+                    print("Useage Warning: Overwriting set x0 with random values.")
+            elif type(self.x0) == int and self.x0!=0:
+                print(f"Useage Warning: Overwriting set x0={x0} with random values")
+
         else:
-            raise\
-        ValueError("Bad useage: x0 was not of type 'int' or 'numpy.ndarray'")
+            if type(self.x0) == int:
+                if self.x0:
+                    self.xsol = np.ones(self.X.shape[1])*self.x0
+                else:
+                    self.xsol=np.zeros(self.X.shape[1])
+            elif type(self.x0) == np.ndarray:
+                if self.x0.shape[0]==self.X.shape[1]: # if len = p
+                    self.xsol = self.x0
+                else:
+                    raise ValueError("Bad useage: x0 was not of length 1 or p.")
+            else:
+                raise\
+            ValueError("Bad useage: x0 was not of type 'int' or 'numpy.ndarray'")
 
-    # calculate the first step
-    p = calculate_p(X, xsol)
-    dF = delF(X, y, p)
-    step = gamma_k * dF
+        # calculate the first step
+        self.calculate_p()
+        self.delF()
+        self.step = self.gamma_k * self.dF
 
-    i = 0
-    while i <= max_iter:
-        xsol = xsol - step
-        # calculate the next step
-        p = calculate_p(X, xsol)
-        dF = delF(X, y, p)
-        step = gamma_k*dF
-        if np.linalg.norm(step) <= tol:
-            print("GD reached tolerance.")
-            break
-        i += 1
+        i = 0
+        while i <= self.max_iter:
+            self.xsol = self.xsol - self.step
+            # calculate the next step
+            self.calculate_p()
+            self.delF()
+            self.step = self.gamma_k*self.dF
+            if np.linalg.norm(self.step) <= self.tol:
+                print("GD reached tolerance.")
+                break
+            if self.verbose:
+                print(f"{i}\t{np.linalg.norm(self.step)}")
+            i += 1
 
-    if i >= max_iter:
-        print("GD reached max iteration.")
+        if i >= self.max_iter:
+            print("GD reached max iteration.")
 
-    return xsol
 
-def delF(X, y, p):
-    """
-    Calculates an estimation of the gradient of the cost function F.
+    def delF(self):
+        """
+        Calculates an estimation of the gradient of the cost function F.
 
-    Parameters:
-    -----------
-    x : vec
-        Input which dictates where gradient should work from
+        Parameters:
+        -----------
+        x : vec
+            Input which dictates where gradient should work from
 
-    Returns:
-    --------
-    dF : vec
-        Output of which direction F decreases in.
-    """
-    a = y - p
-    dF = - X.T @ a
-    return dF
+        Returns:
+        --------
+        dF : vec
+            Output of which direction F decreases in.
+        """
+        a = self.y - self.p
+        self.dF = - self.X.T @ a
 
-def calculate_p(X, xsol):
-    """
-    Calculates the probability vector using the sigmoid function.
-    """
-    fac = X @ xsol
-    p = 1./(1+np.exp(-fac)) # np.exp(fac)(1+np.exp(fac))is strange
-    return p
+    def calculate_p(self):
+        """
+        Calculates the probability vector using the sigmoid function.
+        """
+        fac = self.X @ self.xsol
+        self.p = 1./(1+np.exp(-fac)) # np.exp(fac)(1+np.exp(fac))is strange
 
-def CGMethod(X, y, x0=0, random_state_x0=False,\
-    gamma_k = 0.01, max_iter=50, tol=1e-3):
-    """
-    Conjugate Gradient method for finding solution of non-linear problems.
-    Reduces residual error r=b-Ax iteratively. Exact solution yields r=0.
-    Constrain that the matrix A is positive definite and symmetric.
-
-    Parameters:
-    -----------
-    X : mtx
-        (N x p) matrix of predictors
-    y : vec
-        (N x 1) vector of targets
-    x0 : vec, default np.zeros(p)
-        (p x 1) Initial guess solution.
-    random_state_x0 : bool, default False
-        If True, set x0 to have elements in [-0.7, 0.7] randomly.
-    gamma_k : float, default 0.01
-        Learning rate for the descent.
-    max_iter : int, default 50
-        Maximum amount of iterations which the gradient descent will perform.
-    tol : float, default 0.001
-        Tolerance for which the iterative process will continue.
-
-    Returns:
-    --------
-    xsol : vec
-        (p x 1) vector solution to b - Ax
-    """
-
-    if random_state_x0:
-        len = X.shape[1] # p
-        xsol = (np.random.random(len) - 0.5)*0.7/0.5 # between [-0.7, 0.7]
-        if x0 != 0:
-            print(f"Warning:\n\tRandom state is overwriting the set x0={x0}.")
-    else:
-        xsol = x0
-
-    i=0
-    while i<max_iter:
-        # A = What is its relation to X?
-        r = y - A @ xsol
-        den = r.T@r
-        num = r.T @ (A @ r)
-        alpha = (den)/(num)
-        xsol -= alpha @ r
-        if np.linalg.norm(xsol) < tol:
-            print(f"Tolerance {tol} reached.")
-            break
-        i+=1
-
-    if i>=max_iter:
-        print(f"Max iteration {i} reached.")
-
-    return xsol
+    def predict(self, Xt, yt):
+        yp = Xt @ obj.xsol
+        a = fns.assert_binary_accuracy(yt, yp)
+        print(f"GDS had accuracy of {100*a:.0f} %")
 
 if __name__ == '__main__':
     pass
